6db8cf7724a06375d50faaec60c7904b53c5ad5d
Sneha Chavan <snchavan1515@gmail.com>
2025-06-18 20:03:06 +0530

    Update next.config.js and vercel.json for deployment

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    serverActions: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  reactStrictMode: true,
-  swcMinify: true,
+  }
 }
 
 module.exports = nextConfig 
